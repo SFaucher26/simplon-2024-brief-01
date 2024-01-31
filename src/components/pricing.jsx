34ca2369cc9230c256b990f700e2786296b77681
FAUCHER Sandrine <sandrine.faucher@maif.fr>
2024-01-31 21:57:15 +0100

    ajout du component Pricing

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -1,2 +1,36 @@
 import React from 'react';
+import '../design/Pricing.css';
 
+
+export default function Pricing() {
+    return (
+        <section id="prices" className="prices">
+            <h2>Nos abonnements</h2>
+            <div className="abonnements">
+            <div className="abonnement">
+                <h3>STARTER</h3>
+                <p>Nom de domaine gratuit</p>
+                <p>Migration assistée</p>
+                <p><strong>2,50 € / mois </strong></p>
+            </div>
+            <div className="abonnement" >
+                <h3>CONFORT</h3>
+                <p>Nom de domaine gratuit</p>
+                <p>Migration assistée</p>
+                <p>Assistance 24/24 - 7/7</p>
+                <p><strong>10€ / mois</strong></p>
+            </div>
+            <div className="abonnement">
+                <h3>PREMIUM</h3>
+                <p>Nom de domaine + SSL gratuit </p>
+                <p>Migration assistée</p>
+                <p>Assistance 24/24 - 7/7</p>
+                <p><strong>15€ / mois </strong></p>
+            </div>
+            </div>
+        </section>
+    )
+
+
+
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import Features from '../components/Features';
 import Footer from '../components/Footer';
+import Pricing from '../components/Pricing';
 
 export default function Home() {
   return (<>
     <Header />
     <main>
       <Features />
+      <Pricing />
     </main>
     <Footer />
   </>);
